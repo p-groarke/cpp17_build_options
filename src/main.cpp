0271d6b0f6f2c019bed729fe91d63fca53c73067
Philippe Groarke <philippe.groarke@gmail.com>
2017-11-19 15:07:57 -0500

    Add example.

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include "runtime_options.h"
+#include <build_options.h>
+
+#include <iostream>
+
+int main(int, char**) {
+	runtime_options.a_launch_option = true;
+
+	if (runtime_options.a_launch_option) {
+		std::cout << "Do runtime things." << std::endl;
+	}
+
+	if constexpr (build_options.my_bool) {
+		std::cout << "Bool is set." << std::endl;
+
+		if constexpr (build_options.my_int == 42) {
+			std::cout << "  Do something with int." << std::endl;
+		}
+		if constexpr (build_options.my_string == "a string") {
+			std::cout << "  Do something with string : "
+					  << build_options.my_string << std::endl;
+		}
+	}
+
+	if constexpr (build_options.cpp_version != CppVersion::cpp17) {
+		std::cout << "Well... This wouldn't compile now would it."
+				  << "Maybe this will become relevant in 20 years? ;)"
+				  << std::endl;
+	}
+
+	return 0;
+}
---
+++ src/runtime_options.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct RuntimeOptions {
+	bool a_launch_option = false;
+};
+inline RuntimeOptions runtime_options;
